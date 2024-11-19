a1fc9cded91123e047fcab9f95d09abacff7e906
bitacademy-poscodx09 <bitacademy.poscodx@gmail.com>
2024-11-19 16:35:27 +0900

    sggle

--- practice01/src/main/java/prob01/Sol01.java
+++ practice01/src/main/java/prob01/Sol01.java
@@ -4,12 +4,11 @@ import java.util.Scanner;
 
 public class Sol01 {
 	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);		
+		Scanner scanner = new Scanner(System.in);
 		int number = scanner.nextInt();
-		
+
 		System.out.println(number);
-		
-		
+
 		scanner.close();
 	}
 }
