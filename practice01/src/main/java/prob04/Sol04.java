72669bbc626b0d736d5580d33da94ef8260c6656
포스코DX 청년 IT 전문가 9기 <bitacademy.poscodx@gmail.com>
2024-11-20 16:46:33 +0900

    Update Sol04.java

--- practice01/src/main/java/prob04/Sol04.java
+++ practice01/src/main/java/prob04/Sol04.java
@@ -7,10 +7,18 @@ public class Sol04 {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 
-		String str = scanner.nextLine();
-		System.out.println(str.length());
-		System.out.println(str.charAt(1));
-		
+		System.out.print("문자열을 입력하세요 : ");
+		String text = scanner.nextLine();
+
+		int length = text.length();
+		for(int i = 0; i < length; i++) {
+			for(int j = 0; j <= i; j++) {
+				System.out.print(text.charAt(j));
+			}
+
+			System.out.println("");
+		}
+
 		scanner.close();
 	}
 }
