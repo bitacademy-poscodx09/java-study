6d2497eb8a953cae3fc9e4a092cda412f3d07d72
bitacademy-poscodx09 <bitacademy.poscodx@gmail.com>
2024-11-21 10:45:33 +0900

    java study #1

---
+++ practice02/src/main/java/prob02/Sol02.java
@@ -0,0 +1,16 @@
+package prob02;
+
+import java.util.Scanner;
+
+public class Sol02 {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+
+		int[] intArray = new int[5];
+		double sum = 0;
+
+		/* 코드 작성 */
+		
+		scanner.close();
+	}
+}
---
+++ practice02/src/main/java/prob05/Sol05.java
@@ -0,0 +1,25 @@
+package prob05;
+
+public class Sol05 {
+	public static void main(String[] arg) {
+
+		int array[] = {5, 9, 3, 8, 60, 20, 1};
+		int count = array.length;
+
+		System.out.println("Before Sort.");
+
+		for (int i = 0; i < count; i++) {
+			System.out.print(array[i] + " ");
+		}
+
+		//
+		// 정렬 알고리즘이 적용된 코드를 여기에 작성합니다.
+		//
+
+		System.out.println("\nAfter Sort.");
+
+		for (int i = 0; i < count; i++) {
+			System.out.print(array[i] + " ");
+		}
+	}
+}
