7eca81f35d5d03669640e27e0689fae0a91dc00c
포스코DX 청년 IT 전문가 9기 <bitacademy.poscodx@gmail.com>
2024-11-21 16:22:53 +0900

    Update GoodsTest.java

--- practice03/src/main/java/prob02/GoodsTest.java
+++ practice03/src/main/java/prob02/GoodsTest.java
@@ -6,17 +6,31 @@ public class GoodsTest {
 	private static final int COUNT_GOODS = 3;
 
 	public static void main(String[] args) {
+
 		Scanner scanner = new Scanner(System.in);
 
 		Goods[] goods = new Goods[COUNT_GOODS];
 
-		// 상품 입력
-		String line = scanner.nextLine();
-		String[] info = line.split(" ");
-		
-		
-		// 상품 출력
-		
+		for (int i = 0; i < COUNT_GOODS; i++) {
+			String line = scanner.nextLine();
+
+			String[] tokens = line.split(" ");
+			String name = tokens[0];
+			int price = Integer.parseInt(tokens[1]);
+			int countStock = Integer.parseInt(tokens[2]);
+
+			Goods g = new Goods();
+			g.setName(name);
+			g.setPrice(price);
+			g.setCountStock(countStock);
+
+			goods[i] = g;
+		}
+
+		for (Goods g : goods) {
+			System.out.println(g.getName() + "(가격:" + g.getPrice() + "원)이 " + g.getCountStock() + "개 입고 되었습니다.");
+		}
+
 		scanner.close();
 	}
 }
