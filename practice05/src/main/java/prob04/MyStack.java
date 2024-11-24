330e4ec65c862b442ba4427d40c89b997b3a13d1
bitacademy-poscodx09 <bitacademy.poscodx@gmail.com>
2024-11-24 12:33:36 +0900

    java study #8 - practice04/05 solved

--- practice05/src/main/java/prob04/MyStack.java
+++ practice05/src/main/java/prob04/MyStack.java
@@ -5,24 +5,39 @@ public class MyStack {
 	private String[] buffer;
 
 	public MyStack(int capacity) {
-		/* 구현하기 */
+		top = -1;
+		buffer = new String[capacity];
 	}
 
 	public void push(String s) {
-		/* 구현하기 */
+		if (top == buffer.length - 1) {
+			resize();
+		}
+
+		buffer[++top] = s;		
 	}
 
 	public String pop() throws MyStackException {
-		/* 구현하기 */
-		return null;
+		if (isEmpty()) {
+			throw new MyStackException("stack is empty");
+		}
+
+		String result = buffer[top];
+		buffer[top--] = null;
+
+		return result;
 	}
 
 	public boolean isEmpty() {
-		/* 구현하기 */
-		return false;
+		return top == -1;
 	}
 
 	private void resize() {
-		/* 구현하기 */
-	}	
+		String[] temp = new String[buffer.length * 2];
+		for (int i = 0; i <= top; i++) {
+			temp[i] = buffer[i];
+		}
+
+		buffer = temp;
+	}
 }
